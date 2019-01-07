4f1537eca3a3f81f2ac5ed4388737fd58b40ff32
Jan Küster <jkuester@uni-bremen.de>
2019-01-07 10:09:18 +0100

    Eventmap scoping (#3)
    * Tests added to ensure proper eventmap scoping when teleporting
    
    * Added missing semicolons
    
    * Tests template proper indentation

--- src/teleport.tests.js
+++ src/teleport.tests.js
@@ -4,9 +4,18 @@ import { assert } from 'meteor/practicalmeteor:chai';
 import { withRenderedTemplate } from './helpers.tests';
 import './teleport.tests.html';
 
+let eventMapScoped;
+
+Template.dest.events({
+  'click #click-target' (event, templateInstance) {
+    event.preventDefault();
+    eventMapScoped = true;
+  }
+})
+
 const isBeamed = (oldTarget, newTarget, selector) => {
-  assert.equal($(oldTarget).find(selector).length, 0)
-  assert.equal($(newTarget).find(selector).length, 1)
+  assert.equal($(oldTarget).find(selector).length, 0);
+  assert.equal($(newTarget).find(selector).length, 1);
 }
 
 describe('teleport to render destination', function () {
@@ -22,33 +31,46 @@ describe('teleport to render destination', function () {
   it ('renders a Template with a given target node as destination', function (done) {
     // let's append a new div to the body
     // which will act as our render target
-    const targetNode = $('<div>').prop('id', 'target-node')
-    $('body').append(targetNode)
+    const targetNode = $('<div>').prop('id', 'target-node');
+    $('body').append(targetNode);
 
     // now let's beam the template to the target
     withRenderedTemplate('dest', {destination: targetNode.get(0)}, (el) => {
-      isBeamed(el, '#target-node', '#test-target-id')
-      done()
+      isBeamed(el, '#target-node', '#test-target-id');
+      done();
     })
   });
 
   it ('renders a Template with a given CSS selector as destination', function (done) {
     // let's create a div with a specific CSS class
     // and add it to the body
-    const targetNode = $('<div>').addClass('target-node')
-    $('body').append(targetNode)
+    const targetNode = $('<div>').addClass('target-node');
+    $('body').append(targetNode);
 
     // now let's beam the template to the target by CSS selector
     withRenderedTemplate('dest', {destination: '.target-node'}, (el) => {
-      isBeamed(el, '.target-node', '#test-target-id')
-      done()
+      isBeamed(el, '.target-node', '#test-target-id');
+      done();
     })
   });
 
   it ('renders a Template in the document.body as default destination', function (done) {
     withRenderedTemplate('dest', {}, (el) => {
-      isBeamed(el, 'body', '#test-target-id')
-      done()
+      isBeamed(el, 'body', '#test-target-id');
+      done();
     })
+  });
+
+  it ('keeps the eventmap in scope when rendering to a destination', function (done) {
+    withRenderedTemplate('dest', {}, (el) => {
+      const clickTarget = $('#click-target');
+      clickTarget.on('click', () => {
+        assert.isTrue(eventMapScoped);
+        done();
+      });
+      // We need to simulate a mouse click
+      // on the actual DOM element
+      clickTarget[0].click();
+    });
   });
 });
